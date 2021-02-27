dde8e8617e19dcd1edb392bf9b4d8d48cb0a0929
liuhengzhix <l2731286559@163.com>
2021-02-27 20:02:14 +0800

    [#1]关联

--- IncludeDemo/main.c
+++ IncludeDemo/main.c
@@ -2,13 +2,6 @@
 #include "my.h"
 int main()
 {
-    // TODO:编译未通过
     printf("%d\n", sum(1, 100));
     return 0;
 }
-/*
-    cd "d:\Program\vscode\cpp\IncludeDemo\" && g++ main.cpp -o
-    g++ main.cpp -o main && "d:\Program\vscode\cpp\IncludeDemo\"main 
-    d:/program/minggw/bin/../lib/gcc/mingw32/9.2.0/../../../../mingw32/bin/ld.exe: C:\Users\LHZ\AppData\Local\Temp\ccOpzP5q.o:main.cpp:(.text+0x1e): undefined reference to `sum(int, int)'
-    collect2.exe: error: ld returned 1 exit status
-*/
